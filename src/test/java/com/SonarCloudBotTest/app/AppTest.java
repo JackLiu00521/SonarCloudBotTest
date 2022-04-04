84aefb9ddfb39a2575cb4ecf49a115d06cf91c26
jacliu1996 <jacliu1996@gmail.com>
2022-04-04 21:50:21 +0800

    Update AppTest.java

--- src/test/java/com/SonarCloudBotTest/app/AppTest.java
+++ src/test/java/com/SonarCloudBotTest/app/AppTest.java
@@ -22,13 +22,12 @@ public class AppTest
     @Test
     public void testAddNums() {
         // 1. Arrange
-        Calculator calculator = new Calculator();
         int number1 = 5;
         int number2 = 10;
         int excepted = 15;
 
         // 2. Act
-        int actual = calculator.add(number1, number2);
+        int actual = addNums(number1, number2);
 
         // 3. Assert
         assertThat(actual).isEqualTo(excepted);
