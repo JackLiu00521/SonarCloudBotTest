239c643d2e13ed71ac19447a8ab61fb006fdc164
jacliu1996 <jacliu1996@gmail.com>
2022-04-04 22:22:30 +0800

    Update AppTest.java

--- src/test/java/com/SonarCloudBotTest/app/AppTest.java
+++ src/test/java/com/SonarCloudBotTest/app/AppTest.java
@@ -1,7 +1,6 @@
 package com.SonarCloudBotTest.app;
 
-import static org.junit.Assert.assertTrue;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert;
 import org.junit.Test;
 
 /**
